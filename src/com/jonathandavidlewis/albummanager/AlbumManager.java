a1aa66dbc12379921fd410fe111ee77267bead64
Jonathan Lewis <jonathandavidlewis@gmail.com>
2018-03-20 00:20:30 -0700

    creates importAlbums method to decouple

--- src/com/jonathandavidlewis/albummanager/AlbumManager.java
+++ src/com/jonathandavidlewis/albummanager/AlbumManager.java
@@ -15,8 +15,40 @@ public class AlbumManager {
         System.out.println("This will sort the albums in a file.");
         String filePath = promptUserInput("Please enter the full or relative file path of your album list, then press ENTER: ");
 
-        // Read from the file
+        List<Album> list = importAlbums(filePath);
 
+        Collections.sort(list);
+
+        String outputFilePath = promptUserInput("Please enter the full or relative output file path, then press ENTER: ");
+
+        // Save the file
+
+        StringBuffer outputText = new StringBuffer();
+
+        for(Album album : list) {
+            String albumEntryAsString = album.toString() + "\n";
+            outputText.append(albumEntryAsString);
+        }
+
+        Path path = Paths.get(outputFilePath);
+        byte[] strToBytes = outputText.toString().getBytes();
+
+        try {
+            Files.write(path, strToBytes);
+        } catch (IOException ex) {
+            System.out.println("Error writing file.");
+        }
+
+        System.out.println(outputText);
+        System.out.println("Your albums have been output to: " + path.toAbsolutePath());
+    }
+
+    private static String promptUserInput(String promptText) {
+        System.out.println(promptText);
+        Scanner reader = new Scanner(System.in);
+        return reader.next();
+    }
+    private static List<Album> importAlbums(String filePath) {
         String textLine = null;
 
         List<Album> list = new ArrayList<Album>();
@@ -43,46 +75,15 @@ public class AlbumManager {
 
                 list.add(thisAlbum);
                 lineNumber++;
-
             }
-
             bufferedReader.close();
-
-            // Sort the list by predefined pattern
-
-            Collections.sort(list);
-
-            StringBuffer outputText = new StringBuffer();
-
-            for(Album album : list) {
-                String albumEntryAsString = album.toString() + "\n";
-                outputText.append(albumEntryAsString);
-            }
-
-            String outputFilePath = promptUserInput("Please enter the full or relative output file path, then press ENTER: ");
-
-            // Save the file
-
-            Path path = Paths.get(outputFilePath);
-            byte[] strToBytes = outputText.toString().getBytes();
-
-            Files.write(path, strToBytes);
-
-            System.out.println(outputText);
-
-            System.out.println("Your albums have been output to: " + path.toAbsolutePath());
 
         } catch (FileNotFoundException ex) {
             System.out.println("Unable to open file.");
         } catch (IOException ex) {
             System.out.println("Error reading file.");
         }
-    }
 
-    private static String promptUserInput(String promptText) {
-        System.out.println(promptText);
-        Scanner reader = new Scanner(System.in);
-        return reader.next();
+        return list;
     }
-
 }
